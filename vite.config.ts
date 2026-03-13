556334657d4b5d59b38f36d0e215423391bc3b1a
KevinBal <kevin.balavoine@simphonis.com>
2026-03-13 11:11:41 +0100

    add github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig((env: ConfigEnv) => ({
-    plugins: [tailwindcss(), react()],
-    base: env.command === "build" ? "/" : "/",
+  plugins: [tailwindcss(), react()],
+  base: env.command === "build" ? "/Pignat_Jumeau_Numerique_Recette/" : "/",
 }));
